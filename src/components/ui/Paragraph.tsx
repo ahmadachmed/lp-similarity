c17178660d03bec51ab903c7e9ba4ef72f41d892
Ahmad Ilham <ahmad.ilham@amartha.com>
2023-03-14 23:19:27 +0700

    first commit

---
+++ src/components/ui/Paragraph.tsx
@@ -0,0 +1,46 @@
+import { FC, forwardRef, HTMLAttributes } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const paragraphVariants = cva(
+  "max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center",
+  {
+    variants: {
+      size: {
+        default: "text-base sm:text-lg",
+        sm: "text-sm sm:text-base",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+);
+
+interface LargeHeadingProps
+  extends HTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof paragraphVariants> {}
+
+// eslint-disable-next-line react/display-name
+const Paragraph = forwardRef<HTMLParagraphElement, LargeHeadingProps>(
+  ({ className, size, children, ...props }, ref) => {
+    return (
+      <p
+        ref={ref}
+        {...props}
+        className={cn(
+          paragraphVariants({
+            size,
+            className,
+          })
+        )}
+      >
+        {children}
+      </p>
+    );
+  }
+);
+
+Paragraph.displayName = 'Paragraph'
+
+export default Paragraph;
